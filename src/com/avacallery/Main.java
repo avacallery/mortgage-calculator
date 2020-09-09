98f08ac7dd5fb0a35c437daeb4a7bd8da3e8813f
avacallery <avacallery@gmail.com>
2020-09-09 14:41:03 -0400

    refactor main method

--- src/com/avacallery/Main.java
+++ src/com/avacallery/Main.java
@@ -12,20 +12,25 @@ public class Main {
 	    float annualInterest = (float)readNumber("Annual Interest Rate: ", 1, 30);
 	    byte years = (byte)readNumber("Years: ", 1, 30);
 
-	    double mortgage = calculateMortgage(principal, annualInterest, years);
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+    }
 
+    private static void printMortgage(int principal, float annualInterest, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("-----------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
 
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("------------");
         for (short month = 1; month <= years * MONTHS_PER_YEAR; month++) {
             double balance = calculateBalance(principal, annualInterest, years, month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-
         }
     }
 
