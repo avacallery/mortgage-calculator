be2257799d6d8509ef5002ef394e32957cc00b89
avacallery <avacallery@gmail.com>
2020-09-13 13:13:15 -0400

    static Scanner object

--- src/com/avacallery/Console.java
+++ src/com/avacallery/Console.java
@@ -3,12 +3,21 @@ package com.avacallery;
 import java.util.Scanner;
 
 public class Console {
+    // every time we call readNumber, it creates a new instance of the Scanner class
+    // so, we declare Scanner as a private static field
+    // it is only used when this method(s) are called
+    // WE CAN USE THIS CLASS WHENEVER WE WANT TO READ A VALUE FROM THE CONSOLE
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
         while (true) {
             System.out.println(prompt);
-            value = scanner.nextFloat();
+            value = scanner.nextDouble();
             if (value >= min && value <= max)
                 break;
             System.out.println("Please enter a value between " + min + " and " + max);
