f0ccf749377bf47a49f29cbfabe53f9d92cbdfe6
avacallery <avacallery@gmail.com>
2020-09-13 14:00:36 -0400

    reduce coupling in MortgageCalculator

--- src/com/avacallery/MortgageCalculator.java
+++ src/com/avacallery/MortgageCalculator.java
@@ -1,15 +1,26 @@
 package com.avacallery;
 
 public class MortgageCalculator {
-    public static double calculateBalance(
-            int principal,
-            float annualInterest,
-            byte years,
-            short numberOfPaymentsMade
-    ) {
+    public final static byte MONTHS_PER_YEAR = 12;
+    public final static byte PERCENT = 100;
 
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_PER_YEAR;
-        short numberOfPayments = (short)(years * Main.MONTHS_PER_YEAR);
+    //encapsulate instance fields
+    // the data that we store here represent the state of our calculator object
+    private int principal;
+    private float annualInterest;
+    private byte years;
+
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        // initialize private fields
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
+    public double calculateBalance(short numberOfPaymentsMade) {
+
+        float monthlyInterest = getMonthlyInterest();
+        short numberOfPayments = (short)(getNumberOfPayments());
 
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -18,18 +29,30 @@ public class MortgageCalculator {
         return balance;
     }
 
-    public static double calculateMortgage(
-            int principal,
-            float annualInterest,
-            byte years) {
 
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_PER_YEAR;
-        short numberOfPayments = (short)(years * Main.MONTHS_PER_YEAR);
+    public double calculateMortgage() {
+
+        float monthlyInterest = getMonthlyInterest();
+        short numberOfPayments = (short) getNumberOfPayments();
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1 );
 
         return mortgage;
+    }
+
+    // these two methods are private bc of implementation detail
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_PER_YEAR;
+    }
+
+    private int getNumberOfPayments() {
+        return years * MONTHS_PER_YEAR;
+    }
+
+    // getYears is public because other classes need to use it
+    public short getYears() {
+        return years;
     }
 }
--- src/com/avacallery/MortgageReport.java
+++ src/com/avacallery/MortgageReport.java
@@ -3,21 +3,30 @@ package com.avacallery;
 import java.text.NumberFormat;
 
 public class MortgageReport {
-    public static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = MortgageCalculator.calculateMortgage(principal, annualInterest, years);
+    // this class delegates all the responsibility of calculations
+    // it should only be responsible for presentation
+    private MortgageCalculator calculator;
+
+    // constructor initializes the calculator field
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("------------");
+        for (short month = 1; month <= calculator.getYears() * MortgageCalculator.MONTHS_PER_YEAR; month++) {
+            double balance = calculator.calculateBalance(month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("-----------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
-    }
-
-    public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("------------");
-        for (short month = 1; month <= years * Main.MONTHS_PER_YEAR; month++) {
-            double balance = MortgageCalculator.calculateBalance(principal, annualInterest, years, month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
     }
 }
